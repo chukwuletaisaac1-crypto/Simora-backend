f4ebca3cd04f0275b7e61e949fc959a49f9ae5bc
chukwuletaisaac1-crypto <chukwuletaisaac1@gmail.com>
2026-06-01 05:08:58 +0100

    Fix import statement for executeSimoraCoreEngine

--- Server.ts
+++ Server.ts
@@ -1,4 +1,4 @@
-import { executeSimoraCoreEngine.ts } from './engines/executeSimoraCoreEngine.ts';
+import { executeSimoraCoreEngine } from "./engines/executeSimoraCoreEngine";0
 import express, { Request, Response } from 'express';
 import { createClient } from '@supabase/supabase-js';
 import { Queue, Worker, Job } from 'bullmq';
