9ababd6300fa9423b5f5b1a4ed5cd198e80ff042
chukwuletaisaac1-crypto <chukwuletaisaac1@gmail.com>
2026-06-06 20:50:58 +0100

    Update Server.ts

--- Server.ts
+++ Server.ts
@@ -1,3 +1,5 @@
+import dns from 'dns';
+dns.setDefaultResultOrder('ipv4first');
 import express, { Request, Response } from 'express';
 import { Queue, Worker, Job } from 'bullmq';
 import crypto from 'crypto';
